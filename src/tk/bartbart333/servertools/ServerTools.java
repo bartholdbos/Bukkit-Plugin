397e8a9426d0ca54acde166121f41b7311c68474
bartbart333 <bartholdbos@hotmail.com>
2014-08-23 01:40:42 +0200

    ServerTools add onEnable() and onDisable()

--- src/tk/bartbart333/servertools/ServerTools.java
+++ src/tk/bartbart333/servertools/ServerTools.java
@@ -1,7 +1,15 @@
 package tk.bartbart333.servertools;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerTools extends JavaPlugin{
 	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
 }
