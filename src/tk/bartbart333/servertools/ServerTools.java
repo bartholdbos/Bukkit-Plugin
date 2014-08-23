eab85449ff6353b0506efa93ee6dcbb3a9546ede
bartbart333 <bartholdbos@hotmail.com>
2014-08-23 13:54:57 +0200

    Implemented BB-Config

--- src/tk/bartbart333/servertools/ServerTools.java
+++ src/tk/bartbart333/servertools/ServerTools.java
@@ -3,10 +3,17 @@ package tk.bartbart333.servertools;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import tk.bartbart333.config.Config;
+
 public class ServerTools extends JavaPlugin{
 	
+	private Config config = new Config(getDataFolder() + "/ServerTools");
+		
 	public void onEnable(){
 		
+		
+		config.load();
+		config.store();
 	}
 	
 	public void onDisable(){
